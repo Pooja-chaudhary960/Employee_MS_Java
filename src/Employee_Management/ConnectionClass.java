8a3d05cecdbceeee2b357a49fe132814e9a9b076
Pooja-chaudhary960 <pujadahit52@gmail.com>
2026-03-15 17:21:28 +0545

    Initial commit

---
+++ src/Employee_Management/ConnectionClass.java
@@ -0,0 +1,26 @@
+package Employee_Management;
+
+import java.sql.*;
+
+public class ConnectionClass {
+    Connection con;
+    Statement stm;
+
+    // Constructor to establish connection
+    ConnectionClass() {
+        try {
+         
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            // Establishing connection to MySQL
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3307/Employee_Management", "root", "Pooja@123");
+            System.out.println("Connection established successfully!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Main method to run the class
+    public static void main(String[] args) {
+        new ConnectionClass();  //  instance of ConnectionClass
+    }
+}
